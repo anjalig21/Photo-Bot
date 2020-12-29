04014b31a83e11f2943e9b1231ced8744cc48353
Anjali Gupta <anjaligta@outook.com>
2020-12-28 20:34:33 -0500

    added a new file for image function

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
 // General Initializations 
+require('dotenv').config();
+const func_img = require('./func_img.js')
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
-const cheerio = require ('cheerio');
-
-const request = require('request');
-
 
 // Looks for '!' in order to allow a command 
 const prefix = '!';
-
 
 // Outputs 'Discord Bot is online!' when online
 client.once('ready', () => {
     console.log('Discord Bot is online!');
 });
-
 
 // Different Commands 
 client.on('message', message =>{
@@ -26,51 +20,17 @@ client.on('message', message =>{
     
     const command = args[0];
 
-    function multiple(args){
-        for ()
+    response_text = "";
+
+    for (var i = 1; i < args.length; i++){
+        response_text = response_text + " " + args[i];
     }
 
-if (command === 'image'){
-    image(message, args[1]);
-}
+    if (command === 'image'){
+        func_img.image(message, response_text);
+    }
 });
 
-
-// object that collects randomized images from a search engine 
-function image(message, args){
-    var options = {
-        url: "http://results.dogpile.com/serp?qc=images&q=" + args,
-        method: "GET",
-        headers: {
-            "Accept": "text/html",
-            "User-Agent": "Chrome"
-        }
-    };
-    
-    request(options, function(error, response, responseBody) {
-        if (error) {
-            return;
-        }
-
-        $ = cheerio.load(responseBody);
-
-        var links = $(".image a.link");
-
-        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
-
-        console.log(urls);
-
-        if (!urls.length) {
-            return;
-        }
-
-
-        // Send result
-        message.channel.send(urls[Math.floor(Math.random() * urls.length)]);
-    });
-    
-}
-
 // Token
-client.login('NzkxNTI0NDg4Njg0MjQwOTQ2.X-Qanw.07WOXGyFE3psQ5xbTd-zqBFkKfw');
+client.login(process.env.TOKEN);
 
